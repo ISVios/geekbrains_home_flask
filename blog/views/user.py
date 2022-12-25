4c7e9adb333994fa7ee2366708f610c96e7d73e8
vios <i.svoboda.vios@gmail.com>
2022-12-25 04:49:04 +0300

    conver user view to db

--- blog/views/user.py
+++ blog/views/user.py
@@ -1,28 +1,21 @@
 from flask import Blueprint, render_template
 from werkzeug.exceptions import NotFound
 
+from blog.models import UserModel
+
 
 users_app = Blueprint("users_app", __name__)
-USER = {
-    1: "User1",
-    2: "User2",
-    3: "User3",
-    4: "User4",
-    5: "User5",
-}
 
 
 @users_app.route("/", endpoint="list")
 def users_list():
-    return render_template("user/list.html", users=USER)
+    users = UserModel.query.all()
+    return render_template("user/list.html", users=users)
 
 
 @users_app.route("/<int:user_index>/", endpoint="detail")
 def user_detail(user_index: int):
-    try:
-        user_name = USER[user_index]
-    except KeyError:
+    user = UserModel.query.filter_by(id=user_index).one_or_none()
+    if user is None:
         raise NotFound(f"User #{user_index} no found.")
-    return render_template(
-        "user/detail.html", user_index=user_index, user_name=user_name
-    )
+    return render_template("user/detail.html", user=user)
