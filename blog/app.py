8e5c1105d60819a8ae20a578c7c339a2820b3499
vios <i.svoboda.vios@gmail.com>
2022-12-25 03:26:01 +0300

    add sqlalchemy config

--- blog/app.py
+++ blog/app.py
@@ -1,42 +1,27 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from werkzeug.exceptions import BadRequest
 
 
+from blog.models import db
 from blog.views.user import users_app
 from blog.views.article import article_app
 
 app: Flask = Flask(__name__)
 
+
+# __CONFIG__
+
+# __DB__
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/blog.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
+
+# __BLUEPRINT__
 app.register_blueprint(users_app, url_prefix="/users")
 app.register_blueprint(article_app, url_prefix="/articles")
 
 
+# __ROUTE__
 @app.route("/")
 def index():
     return render_template("index.html")
-
-@app.route("/power")
-def power():
-    x = request.args.get("x", "")
-    y = request.args.get("y", "")
-
-    if not (x.isdigit() and y.isdigit()):
-        app.logger.info(f"in '/power' x or y no digit. x={x} y={y}")
-        return BadRequest("No found x or y param")
-
-    x = int(x)
-    y = int(y)
-    result = x**y
-    app.logger.info(f"open '/power' {x} ^ {y} = {result}")
-    return str(result)
-
-
-@app.errorhandler(ZeroDivisionError)
-def handle_zero_div_error(error):
-    app.logger.error(error)
-    return "<h1>Please don't forget close darkhole by youself</h1>", 400
-
-
-@app.route("/inf")
-def inf():
-    return str(1 / 0)
