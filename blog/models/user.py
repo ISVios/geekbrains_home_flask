5ec3197fecd9b30caed7c586c442d9b5614e3b1f
vios <i.svoboda.vios@gmail.com>
2023-01-16 20:29:57 +0300

    add password to user model and migrate db

--- blog/models/user.py
+++ blog/models/user.py
@@ -1,14 +1,27 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, LargeBinary, String, Boolean
 from blog.models import db
 from flask_login import UserMixin
+
+import flask_bcrypt
 
 
 class UserModel(db.Model, UserMixin):
     id = Column(Integer, primary_key=True)
     username = Column(String(80), unique=True, nullable=False)
     email = Column(String(255), nullable=False, default="", server_default="")
+    _password = Column(LargeBinary, nullable=False)
     is_staff = Column(Boolean, nullable=False, default=False)
 
+    @property
+    def password(self):
+        return self._password
 
-def __repr__(self):
-    return f"<User #{self.id} {self.username!r}>"
+    @password.setter
+    def password(self, val):
+        self._password = flask_bcrypt.generate_password_hash(val)
+
+    def password_vildate(self, password):
+        return flask_bcrypt.check_password_hash(self._password, password)
+
+    def __repr__(self):
+        return f"<User #{self.id} {self.username!r}>"
